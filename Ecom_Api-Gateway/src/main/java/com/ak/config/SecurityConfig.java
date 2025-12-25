7bf40e0d9d2a17c1c93ddc4e1acc0180905b6db5
Gogulapati Ashok <124648330+ashoka66@users.noreply.github.com>
2025-12-25 21:26:32 +0530

    New Version Development

--- Ecom_Api-Gateway/src/main/java/com/ak/config/SecurityConfig.java
+++ Ecom_Api-Gateway/src/main/java/com/ak/config/SecurityConfig.java
@@ -2,39 +2,41 @@ package com.ak.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
 import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
+@EnableWebFluxSecurity
 public class SecurityConfig {
 	
 	
-//	@Bean
-//	public SecurityWebFilterChain springsSecurityFilterChain(ServerHttpSecurity http) {
-//		
-//		 http.csrf(ServerHttpSecurity.CsrfSpec::disable)
-//		     .authorizeExchange(ex->ex
-//		    		             .pathMatchers("/auth/**").permitAll()
-//		    		             .pathMatchers("/products/**").authenticated()
-//		    		             .pathMatchers("/orders/**").authenticated()
-//		    		             .anyExchange().authenticated()
-//		    		             );
-//		
-//		
-//		
-//		
-//		return http.build();
-	
-//}
-
 	@Bean
-    public SecurityWebFilterChain springsSecurityFilterChain(ServerHttpSecurity http) {
-        
-        http.csrf(ServerHttpSecurity.CsrfSpec::disable)
-            .authorizeExchange(ex->ex
-                .anyExchange().permitAll()  // Let our custom filter handle auth
-            );
-        
-        return http.build();
-    }
+	public SecurityWebFilterChain springsSecurityFilterChain(ServerHttpSecurity http) {
+		
+		 http.csrf(ServerHttpSecurity.CsrfSpec::disable)
+		     .authorizeExchange(ex->ex
+		    		             .pathMatchers("/auth/**").permitAll()
+		    		             .pathMatchers("/products/**").authenticated()
+		    		             .pathMatchers("/orders/**").authenticated()
+		    		             .anyExchange().authenticated()
+		    		             );
+		
+		
+		
+		
+		return http.build();
+	
+}
+//
+//	@Bean
+//    public SecurityWebFilterChain springsSecurityFilterChain(ServerHttpSecurity http) {
+//        
+//        http.csrf(ServerHttpSecurity.CsrfSpec::disable)
+//            .authorizeExchange(ex->ex
+//                .anyExchange().permitAll()  // Let our custom filter handle auth
+//            );
+//        
+//        return http.build();
+//    }
 }
