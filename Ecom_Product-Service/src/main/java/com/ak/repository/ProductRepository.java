ba21b66cf3c186a9ee94998e9ea5d19336cd1bff
Gogulapati Ashok <124648330+ashoka66@users.noreply.github.com>
2026-01-11 23:54:18 +0530

    Remove Product-Service

--- Ecom_Product-Service/src/main/java/com/ak/repository/ProductRepository.java
+++
@@ -1,9 +0,0 @@
-package com.ak.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.ak.entitiy.Product;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-
-}
