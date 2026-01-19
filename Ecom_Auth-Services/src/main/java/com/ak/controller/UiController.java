742b5b603e2a9014a39d80f28e4e94c47ab9072f
Gogulapati Ashok <124648330+ashoka66@users.noreply.github.com>
2026-01-19 21:44:27 +0530

    Added login view in the uicontroller

--- Ecom_Auth-Services/src/main/java/com/ak/controller/UiController.java
+++ Ecom_Auth-Services/src/main/java/com/ak/controller/UiController.java
@@ -1,10 +1,20 @@
 package com.ak.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class UiController {
 	
-	
+	 @Autowired
+	  private RestTemplate restTemplate;
+
+	  @GetMapping("/login")
+	  public String loginPage() {
+	    return "login";
+	  }
+
 
 }
