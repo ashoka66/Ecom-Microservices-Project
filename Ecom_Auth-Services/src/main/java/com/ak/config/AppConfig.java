cef0485f70efa53fc7f686956b06f55163b74584
Gogulapati Ashok <124648330+ashoka66@users.noreply.github.com>
2026-01-24 23:17:36 +0530

    Added a cookie-forwarding interceptor

--- Ecom_Auth-Services/src/main/java/com/ak/config/AppConfig.java
+++ Ecom_Auth-Services/src/main/java/com/ak/config/AppConfig.java
@@ -3,12 +3,48 @@ package com.ak.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.request.RequestContextHolder;
+
+import jakarta.servlet.http.HttpServletRequest;
+
+import org.springframework.web.context.request.ServletRequestAttributes;
 
 @Configuration
 public class AppConfig {
+//	@Bean
+//	public RestTemplate restTemplate() {
+//		return new RestTemplate();
+//	}
+	
 	@Bean
 	public RestTemplate restTemplate() {
-		return new RestTemplate();
-	}
+		
+		RestTemplate restTemplate= new RestTemplate();
+		
+		 restTemplate.getInterceptors().add((request,body,execution) ->{
+			 
+			 
+			 ServletRequestAttributes attrs=(ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+			 
+			 if(attrs!=null) {
+				 
+				 HttpServletRequest currentRequest=attrs.getRequest();
+				 String cookieHeader=currentRequest.getHeader("Cookie");
+				 
+				 if(cookieHeader!=null) {
+					
+					 request.getHeaders().add("Cookie", cookieHeader)
+					 
+				 }
+				 
+			 }
+			 return execution.execute(request, body);
+			 
+		 });
+		 
+		  return restTemplate;
+		
+	
+	    }
 
 }
